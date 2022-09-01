aa99049a6a135a9bc3c7737969d0f3a84e9646a1
Anna Schmidt <marshall1697@gmail.com>
2022-09-01 10:41:15 -0600

    commit tree 1441dc69307306ba6e136f727acc20b89932417d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+ function writeCards(names, event){
+    let messages = [];
+       for(let i = 0; i< names.length; i++){
+           let newMessage = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+           messages.push(newMessage);
+      }
+     return messages;
+   
+   };
+
+
+   function countDown(int){
+    let count = int;
+    while(count > count -1 && count >= 0){
+        console.log(count--);
+        
+    }
+}
+
